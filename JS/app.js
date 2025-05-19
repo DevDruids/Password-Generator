e188ff8bad8cd22b2256b9f4a7d640e11ab68aa8
Ілля Кравцов <ilya.dmtr.kravtsov@gmail.com>
2025-05-19 15:20:02 +0300

    lol

---
+++ JS/app.js
@@ -0,0 +1,5 @@
+const generatePassword = document.getElementById('generateCard');
+
+generatePassword.addEventListener('click', () => {
+  window.location.href = '../password-generating/passGener.html';
+});
