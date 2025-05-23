d012d2692d906025f419e5291c8875a63600effe
Ілля Кравцов <ilya.dmtr.kravtsov@gmail.com>
2025-05-23 22:14:40 +0300

    password length 2

--- password-generating/passGener.js
+++ password-generating/passGener.js
@@ -1,4 +1,10 @@
 const rangeInput = document.getElementById('inputRange');
+const passwordLengthText = document.getElementById('passwordLength');
+const uppercaseCheckBox = document.getElementById('uppercaseCheckBox');
+const lowerCheckBox = document.getElementById('lowerCheckBox');
+const numbersCheckBox = document.getElementById('numbersCheckBox');
+const symbolsCheckBox = document.getElementById('symbolsCheckBox');
+const generatePasswordBtn = document.getElementById('generatePassword');
 
 function updateBackground() {
   const value = rangeInput.value;
@@ -7,6 +13,12 @@ function updateBackground() {
   const percent = ((value - min) / (max - min)) * 100;
   rangeInput.style.background = `linear-gradient(to right, #80ff00 ${percent}%, #ccc ${percent}%)`;
 }
-
 rangeInput.addEventListener("input", updateBackground);
 updateBackground();
+
+function updatePasswordLength(){
+  passwordLengthText.innerHTML = rangeInput.value;
+}
+
+rangeInput.addEventListener("input", updatePasswordLength);
+updatePasswordLength();
