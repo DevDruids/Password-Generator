749a14a0c0187b8c60fa63da73fa2cba07237bd4
Ілля Кравцов <ilya.dmtr.kravtsov@gmail.com>
2025-05-25 21:47:15 +0300

    password generation 2

--- password-generating/passGener.js
+++ password-generating/passGener.js
@@ -1,10 +1,19 @@
 const rangeInput = document.getElementById('inputRange');
-const passwordLengthText = document.getElementById('passwordLength');
+const passwordLengthNumber = document.getElementById('passwordLength');
 const uppercaseCheckBox = document.getElementById('uppercaseCheckBox');
 const lowerCheckBox = document.getElementById('lowerCheckBox');
 const numbersCheckBox = document.getElementById('numbersCheckBox');
 const symbolsCheckBox = document.getElementById('symbolsCheckBox');
 const generatePasswordBtn = document.getElementById('generatePassword');
+let uppercaseCheckBoxAnswer = ''; 
+let lowerCheckBoxAnswer = '';
+let numbersCheckBoxAnswer = '';
+let symbolsCheckBoxAnswer = '';
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "123456789";
+const symbols = "!@#$%^&*()_+=-`~[]\{}|;':\",./<>?";
+let = characterPool = '';
 
 function updateBackground() {
   const value = rangeInput.value;
@@ -15,31 +24,63 @@ function updateBackground() {
 }
 
 function updatePasswordLength(){
-  passwordLengthText.innerHTML = rangeInput.value;
+  passwordLengthNumber.innerHTML = rangeInput.value;
 }
 
 function checkPasswordOptions(){
-  return;
+  if(uppercaseCheckBoxAnswer) characterPool += upperLetters;
+  if(lowerCheckBoxAnswer) characterPool += lowerLetters;
+  if(numbersCheckBoxAnswer) characterPool += numbers;
+  if(symbolsCheckBoxAnswer) characterPool += symbols;
+  console.log(characterPool)
 }
 
 uppercaseCheckBox.addEventListener('click', () => {
-  uppercaseCheckBox.classList.add('active');
-  if(uppercaseCheckBox.classList === acrive)
+  if(uppercaseCheckBox.classList.contains('active')){
+    uppercaseCheckBox.classList.remove('active');
+    uppercaseCheckBoxAnswer = false;
+  }
+  else{
+    uppercaseCheckBox.classList.add('active');
+    uppercaseCheckBoxAnswer = true;
+  }
+  checkPasswordOptions();
 })
 
 lowerCheckBox.addEventListener('click', () => {
-  lowerCheckBox.classList.add('active');
-  if(uppercaseCheckBox.classList === acrive)
+  if(lowerCheckBox.classList.contains('active')){
+    lowerCheckBox.classList.remove('active');
+    lowerCheckBoxAnswer = false;
+  }
+  else{
+    lowerCheckBox.classList.add('active');
+    lowerCheckBoxAnswer = true;
+  }
+  checkPasswordOptions();
 })
 
 numbersCheckBox.addEventListener('click', () => {
-  numbersCheckBox.classList.add('active');
-  if(uppercaseCheckBox.classList === acrive)
+  if(numbersCheckBox.classList.contains('active')){
+    numbersCheckBox.classList.remove('active');
+    numbersCheckBoxAnswer = false;
+  }
+  else{
+    numbersCheckBox.classList.add('active');
+    numbersCheckBoxAnswer = true;
+  }
+  checkPasswordOptions();
 })
 
 symbolsCheckBox.addEventListener('click', () => {
-  symbolsCheckBox.classList.add('active');
-  if(uppercaseCheckBox.classList === acrive)
+  if(symbolsCheckBox.classList.contains('active')){
+    symbolsCheckBox.classList.remove('active');
+    symbolsCheckBoxAnswer = false;
+  }
+  else{
+    symbolsCheckBox.classList.add('active');
+    symbolsCheckBoxAnswer = true;
+  }
+  checkPasswordOptions();
 })
 
 rangeInput.addEventListener("input", updateBackground);
@@ -47,4 +88,3 @@ updateBackground();
 
 rangeInput.addEventListener("input", updatePasswordLength);
 updatePasswordLength();
-
